91540180d1b04886c59311bdd263afb388c29845
Higor Bueno da Silva <h.higorbsilva.dev@gmail.com>
2021-06-25 23:54:57 -0300

    Create tests Tests not passed

--- test/portnum_test.rb
+++ test/portnum_test.rb
@@ -7,7 +7,21 @@ class PortnumTest < Minitest::Test
     refute_nil ::Portnum::VERSION
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_par
+    assert_respond_to 1, :par?
   end
+
+  def test_par_ok
+    assert 2.par?
+    assert !1.par?
+  end
+
+  def test_impar
+    assert_respond_to 1, :impar?
+  end
+
+  def test_impar_ok
+    assert 1.impar?
+    assert !2.impar?
+  end  
 end
